6203af7eccdb403181506db4ecdb48749de255b0
Sebanti2003 <130296977+Sebanti2003@users.noreply.github.com>
2023-08-30 19:29:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var love=document.querySelector("i");
+var cont=document.querySelector(".main")
+cont.addEventListener("dblclick",function(){
+    love.style.transform ='translate(-50%,-50%) scale(1)';
+    love.style.opacity="0.8";
+    setTimeout(() => {
+        love.style.transform ='translate(-50%,-50%) scale(0)'
+    },1000);
+})
